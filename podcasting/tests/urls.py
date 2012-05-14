d2c2e5435ead5fed5c63b3649f0a59d41ca12c5e
Frank Becker <fb@alien8.de>
2012-05-14 23:28:11 +0200

    Added url_feeds.py to tests/urls.py
        - To test the feeds the urls have to be present.
        - For an totally unknown reason that file is not loaded by the
          nose testrunner - at least for me.

--- podcasting/tests/urls.py
+++ podcasting/tests/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns("",
     url(r"^podcasts/", include("podcasting.urls")),
+    url(r"^podcasts/feeds/", include("podcasting.urls_feeds")),
     )
