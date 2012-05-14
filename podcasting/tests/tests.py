a9359ad4af030d99804f69fdb0009f920951d310
Frank Becker <fb@alien8.de>
2012-05-14 23:25:16 +0200

    Made tests import the urls.py from podcasting explicitly
        - Running manage.py tests podcasting in the project directory
          will use the project urls.py. If it over writes the one from
          podcasting the tests did not run anymore.

--- podcasting/tests/tests.py
+++ podcasting/tests/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 from milkman.dairy import milkman
 
@@ -8,13 +9,22 @@ from podcasting.models import Show, Episode, Enclosure
 
 class PodcastTests(TestCase):
     def setUp(self):
-        self.show = milkman.deliver(Show, title="snowprayers")
-        self.episode = milkman.deliver(Episode, show=self.show, title="Episode 1")
+        settings.ROOT_URLCONF = 'podcasting.tests.urls'
+        self.show = milkman.deliver(
+            Show, title="snowprayers",
+            tags="tag1, tag2",
+        )
+        self.show.save()
+        self.episode = milkman.deliver(Episode, show=self.show,
+                                       title="Episode 1",tags="tag1, tag2")
+        self.episode.save()
         self.enclosure = milkman.deliver(Enclosure, episode=self.episode)
+        self.enclosure.save()
+        self.app_url = reverse("podcasting_show_list")
 
     def test_podcast(self):
         self.assertEquals(self.show, self.enclosure.episode.show)
         self.assertEquals(self.show.get_absolute_url(),
-                          "/podcasts/snowprayers/")
+                          self.app_url + "snowprayers/")
         self.assertEquals(self.episode.get_absolute_url(),
-                          "/podcasts/snowprayers/episode-1/")
+                          self.app_url + "snowprayers/episode/")
