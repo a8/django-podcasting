092c27e1d9c4a90db9aa2408e17b70161f99930f
Frank Becker <fb@alien8.de>
2012-06-23 01:07:22 +0200

    Added test for the view used in admin for category limit

--- podcasting/tests/test_models.py
+++ podcasting/tests/test_models.py
@@ -2,16 +2,16 @@
 
 """UnitTests for all provided feeds
 """
+import copy
+import json
 
 try:
     from django.utils.datetime_safe import datetime
 except ImportError:
     from datetime import datetime
 
-import os
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse
-from django.conf import settings
 
 from milkman.dairy import milkman
 
@@ -45,4 +45,42 @@ class ShowTests(TestCase):
         #self.assertEqual('x', dir(self.show.sub_categories))
         self.assertTrue(self.sub_category in \
                          self.show.sub_categories.all())
+
+    def test_admin_category_helper_view(self):
+        """Test the data injected in the admin form."""
+        response = self.client.get(reverse(
+            "sub_categories_per_main_category",
+            kwargs={'main_category_id': self.main_category.id, 'show_id': self.show.id})
+        )
+        self.assertEquals(200, response.status_code)
+
+        # create new show obj with category data from fixture
+        show_data = copy.deepcopy(self.show_data)
+        main_category = MainCategory.objects.get(name="Technology")
+        sub_category = SubCategory.objects.get(name="Software How-To",
+                                               main_category=main_category)
+        show_data['main_category'] = main_category
+        show_data['sub_categories'] = [sub_category,]
+        show = milkman.deliver(Show, **show_data)
+        show.save()
+        response = self.client.get(reverse(
+            "sub_categories_per_main_category",
+            kwargs={'main_category_id': main_category.id, 'show_id': show.id})
+        )
+        self.assertEquals(200, response.status_code)
+        sub_categories = json.loads(response.content)
+        # get sub_categories names as list
+        sub_categories_per_main_category = SubCategory.objects.filter(
+            main_category=main_category).values_list('name')
+        sub_categories_per_main_category = [n[0] for n in sub_categories_per_main_category]
+
+        for sub_category in sub_categories:
+            # Check if possible sub category belongs to main category
+            self.assertTrue(sub_category[1] in sub_categories_per_main_category)
+            # test for already saved sub categories marked with True
+            # used for html selected attr of multiple choice form in admin
+            if sub_category[1] in [c.name for c in show.sub_categories.all()]:
+                self.assertTrue(sub_category[2])
+            else:
+                self.assertFalse(sub_category[2])
 
